0b181f3c2180ac5c6703be846e9f9a36fd42679e
sorakim <sora6418@gmail.com>
2023-05-07 16:54:25 +0900

    스타일 변경

--- src/form/_styled.js
+++ src/form/_styled.js
@@ -4,6 +4,8 @@ export const JoinFormContainerStyled = styled.div`
   display: flex;
   align-items: center;
   justify-content: center;
+  width: 100vw;
+  height: 100vh;
 `;
 export const JoinFormWrapperStyled = styled.div`
   display: flex;
